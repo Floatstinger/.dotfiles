3313add9e611038b1501d2b820a03a5066ed8ee6
Floatstinger <sndgit@proton.me>
2025-05-26 22:45:35 -0500

    More updates tho.

---
+++ emacs/init.el
@@ -0,0 +1,6 @@
+(use-package exwm
+ :ensure t
+ :config 
+   (require 'exwm')
+   (setq exwm-workspace-number 4)
+ (exwm-enable))
--- setdotfiles.sh
+++ setdotfiles.sh
@@ -3,4 +3,9 @@
 #Set up zsh rc symlink
 ln -sf $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
 
-#Setup emacs config
+#Setup emacs and exwm config
+mkdir -p $HOME/.emacs.d/
+ln -sf $HOME/.dotfiles/emacs/init.el $HOME/.emacs.d/init.el
+
+#Setup xinitrc
+ln -sf $HOME/.dotfiles/xinit/.xinitrc $HOME/.xinitrc
