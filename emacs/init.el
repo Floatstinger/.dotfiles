3db000c62ccd444b2175ccfc0bfe57266d235014
Floatstinger <sndgit@proton.me>
2025-05-26 23:15:01 -0500

    Updated emacs init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,6 +1,11 @@
 (use-package exwm
  :ensure t
  :config 
-   (require 'exwm')
-   (setq exwm-workspace-number 4)
- (exwm-enable))
+   (require 'exwm)
+   (exwm-config-default)
+(exwm-enable))
+
+;;Emacs setup
+(tool-bar-mode -1)
+(display-line-numbers-mode 1)
+(menu-bar-mode -1)
